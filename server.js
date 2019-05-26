3b3035493e9ad51f17558ae979166235586f74d5
oleh.fliurkevych <oleh0809@gmail.com>
2019-05-26 11:55:49 +0300

    Added project structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+// встановлюємо express
+const express = require('express')
+const app = express()
+
+// встановлюємо директорію для віддачі статичного контенту (каталог проекту)
+app.use(express.static(__dirname))
+
+// налаштовуємо роботу із шаблонізаотором
+app.set('views', path.join(__dirname, '/static/views'))
+app.set('view engine', 'pug')
+
+// запускаємо аплікацію
+app.listen(process.env.PORT || 8080)
